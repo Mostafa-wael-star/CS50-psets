33975b09a06c7e1267c9e1b6487ff90e721ad1ff
Mostafa <mostafa.almallah@gmail.com>
2023-10-05 23:49:49 +0300

    added cipher declaration

--- week_2_Arrays/substitution/substitution.c
+++ week_2_Arrays/substitution/substitution.c
@@ -2,9 +2,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+
+string substitute_text(string plainText);
 
 int main(int argc, string argv[])
 {
-    /* code */
-    return 0;
+    if (argc == 1)
+    {
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    else if (argc > 2)
+    {
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 character");
+        return 1;
+    }
+
+    string plainText = get_string("Enter Plain Text : ");
+
+    string cipherText = substitute_text(plainText);
+}
+
+string substitute_text(string plainText)
+{
 }
