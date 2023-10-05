f93c2e4d71ae8df9ed1252c49c334870c9bd7076
Mostafa <mostafa.almallah@gmail.com>
2023-10-05 12:28:43 +0300

    implemented str length and -turn to bin-  function

--- week_2_Arrays/bulbs/bulbs.c
+++ week_2_Arrays/bulbs/bulbs.c
@@ -1,14 +1,26 @@
-#include <cs50.h>
+#include "cs50.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 const int BITS_IN_BYTE = 8;
 
 void print_bulb(int bit);
+int str_length(string s);
+void turn_to_bin(char c);
 
 int main(void)
 {
-    // TODO
+    string s = get_string("please enter the message : ");
+    int length = str_length(s);
+}
+
+int str_length(string s)
+{
+    int length = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+        length++;
+    return length;
 }
 
 void print_bulb(int bit)
@@ -22,5 +34,23 @@ void print_bulb(int bit)
     {
         // Light emoji
         printf("\U0001F7E1");
+    }
+}
+
+void turn_to_bin(char c)
+{
+    char bits[8];
+    int d = c;
+    for (int i = 0; i < BITS_IN_BYTE; i++)
+    {
+        if (d % 2 == 0)
+            bits[i] = '0';
+        else
+            bits[i] = '1';
+        d /= 2;
+    }
+    for (int i = BITS_IN_BYTE - 1; i >= 0; i--)
+    {
+        printf("%c", bits[i]);
     }
 }
