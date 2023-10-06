ce8662ff177f28c7ac34a66e2132c6a314779432
Mostafa <mostafa.almallah@gmail.com>
2023-10-06 11:12:19 +0300

    Fixed argument and key validation

--- week_2_Arrays/substitution/substitution.c
+++ week_2_Arrays/substitution/substitution.c
@@ -5,30 +5,76 @@
 #include <string.h>
 
 string substitute_text(string plainText);
+bool check_valid_args(int argc, string argv);
+bool check_for_duplicates(string argv);
+bool check_for_specials(string argv);
 
 int main(int argc, string argv[])
 {
-    if (argc == 1)
-    {
-        printf("Usage: ./substitution key");
+    if (check_valid_args(argc, argv[1]) == false)
         return 1;
-    }
-    else if (argc > 2)
-    {
-        printf("Usage: ./substitution key");
-        return 1;
-    }
-    if (strlen(argv[1]) != 26)
-    {
-        printf("Key must contain 26 character");
-        return 1;
-    }
 
-    string plainText = get_string("Enter Plain Text : ");
+    string plainText = get_string("plaintext:");
 
     string cipherText = substitute_text(plainText);
+    printf("ciphertext:%s", cipherText);
 }
 
 string substitute_text(string plainText)
 {
+}
+
+bool check_valid_args(int argc, string argv)
+{
+    if (argc == 1 || argc > 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return false;
+    }
+
+    if (strlen(argv) != 26)
+    {
+        printf("Key must contain 26 character\n");
+        return false;
+    }
+
+    if (check_for_specials(argv) == true || check_for_duplicates(argv) == true)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool check_for_duplicates(string argv)
+
+{
+    for (int i = 0; i < strlen(argv); i++)
+        argv[i] = toupper(argv[i]);
+
+    for (int i = 0; i < strlen(argv); i++)
+    {
+        for (int j = i + 1; j < strlen(argv); j++)
+        {
+            if (argv[i] == argv[j])
+            {
+                printf("Key must contain 26 distinguish character\n");
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool check_for_specials(string argv)
+{
+    for (int i = 0; i < strlen(argv); i++)
+    {
+        if (isalpha(argv[i]) == false)
+        {
+            printf("Enter alphabets only\n");
+            return true;
+        }
+    }
+    return false;
 }
